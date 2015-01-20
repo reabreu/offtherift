05dc3afd65b87e0b576ff1a9613a86a5c40b53d4
Paulo Caldeira <paulocaldeira17@gmail.com>
2015-01-20 20:10:38 +0000

    Integration of bootstrap-switch

--- config/env/all.js
+++ config/env/all.js
@@ -16,9 +16,10 @@ module.exports = {
 				'public/lib/bootstrap/dist/css/bootstrap.min.css',
 				'public/lib/font-awesome/css/font-awesome.min.css',
 				'public/lib/iCheck/skins/minimal/blue.css',
-				'public/lib/angular-block-ui/dist/angular-block-ui.min.css'
+				'public/lib/angular-block-ui/dist/angular-block-ui.min.css',
 			],
 			js: [
+				'public/lib/jquery/dist/jquery.min.js',
 				'public/lib/angular/angular.js',
 				'public/lib/angular-resource/angular-resource.js', 
 				'public/lib/angular-cookies/angular-cookies.js', 
@@ -27,13 +28,13 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-				'public/lib/jquery/dist/jquery.min.js',
 				'public/lib/bootstrap/dist/js/bootstrap.min.js',
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
 				'public/lib/jScrollPane/script/jquery.jscrollpane.min.js',
 				'public/lib/iCheck/icheck.min.js',
 				'public/lib/jquery-touchswipe/jquery.touchSwipe.min.js',
 				'public/lib/enquire/dist/enquire.min.js',
+				'public/lib/bootstrap-switch/dist/js/bootstrap-switch.min.js',
 				'public/lib/angular-bootstrap-switch/dist/angular-bootstrap-switch.min.js',
 				'public/lib/angular-block-ui/dist/angular-block-ui.min.js'
 			]
