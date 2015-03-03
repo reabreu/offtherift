e5dd49bed52b8deca5013e583bce2e10ea4bab9f
Rúben Freitas <emanuelfreitas477@gmail.com>
2015-03-03 23:08:11 +0000

    Sincro

---
+++ public/modules/builds/directives/item-section.client.directive.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('builds').directive('itemSection', [
+	function() {
+		return {
+			templateUrl: 'modules/builds/views/item-section.client.view.html',
+			restrict: 'E',
+			controller: function() {
+
+			}
+		};
+	}
+]);
