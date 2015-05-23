2848f79627a9b2f843655293bb1ecf7d7b574a15
Aspherioth <juanmpmacedo@gmail.com>
2015-05-23 16:44:22 +0100

    Production database setup.

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || '52.17.3.23') + '/offtherift',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'ShellOtr:LlehsRt0@52.17.3.23:21345') + '/offtherift',
     app: {
         title: 'Offtherift',
         api: {
