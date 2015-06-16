1f4695f6e5a11a25abc9690aa15b9e18b397c99d
Rúben Freitas <emanuelfreitas477@gmail.com>
2015-06-16 21:03:15 +0100

    URI encode protection

--- public/modules/core/filters/encode-uricomponent.client.filter.js
+++ public/modules/core/filters/encode-uricomponent.client.filter.js
@@ -5,6 +5,9 @@ angular.module('core').filter('encodeUricomponent', [
 		return function(input) {
 			// Encode uricomponent directive logic
 			// ...
+            //
+            if (typeof(input) === 'undefined') return '';
+
             return input.toLowerCase()
                         .replace(/^-+|-+$/g, '')
                         .replace(/[^a-zA-Z0-9]+/g, '-')
