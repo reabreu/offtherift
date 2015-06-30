7371faf07f4cde25b73304b226095f3f04a1973f
Rúben Freitas <emanuelfreitas477@gmail.com>
2015-06-30 22:01:47 +0100

    Basic Auth

--- config/env/production.js
+++ config/env/production.js
@@ -2,7 +2,7 @@
 
 module.exports = {
 	// htaccess protection
-	basicAuth: true,
+	basicAuth: false,
 	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'ShellOtr:LlehsRt0@52.17.3.23:21345') + '/offtherift',
     app: {
         title: 'Offtherift',
